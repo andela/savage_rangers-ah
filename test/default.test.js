f5b252e71a44e4306382ae224af4ab0025f1150b
Frank Mutabazi <frankmutabazi@Franks-MacBook-Pro-2.local>
2019-06-06 18:07:37 +0200

    [Bug 166501135] fix travis bug

--- test/default.test.js
+++ test/default.test.js
@@ -1,8 +1,5 @@
 import chai from 'chai';
 
-
 describe('TravisCI dummy test', () => {
-    it('Should return true', () => {
-          return true;
-    });
+  it('Should return true', () => true);
 });
