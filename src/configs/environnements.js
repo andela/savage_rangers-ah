e79587914165b8e35ef6e6327585ab53c9ce0210
daniel mwangila <daniel.mwangila@andela.com>
2019-06-07 12:23:58 +0200

    [chore #166535780] Enable coveral repoirting and added real test script

--- src/configs/environnements.js
+++ src/configs/environnements.js
@@ -3,46 +3,31 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV;
 
-export default () => {
-  const env = process.env.NODE_ENV;
-
-  switch (env) {
-    case 'test': {
-      return {
-        name: 'test',
-        port,
-        dbUrl: process.env.TEST_DB_URL
-      };
-    }
-
-    case 'dev': {
-      return {
-        name: 'development',
-        port,
-        dbUrl: process.env.DEV_DB_URL
-      };
-    }
-    case 'production': {
-      return {
-        name: 'production',
-        port,
-        dbUrl: process.env.DB_URL
-      };
-    }
-    case 'stage': {
-      return {
-        name: 'stagging',
-        port,
-        dbUrl: process.env.DB_URL
-      };
-    }
-    default: {
-      return {
-        name: 'default',
-        port,
-        dbUrl: process.env.DEV_DB_URL
-      };
-    }
+const environnements = [
+  {
+    name: 'test',
+    port,
+    dbUrl: process.env.TEST_DB_URL
+  },
+  {
+    name: 'development',
+    port,
+    dbUrl: process.env.DEV_DB_URL
+  },
+  {
+    name: 'production',
+    port,
+    dbUrl: process.env.DB_URL
+  },
+  {
+    name: 'stagging',
+    port,
+    dbUrl: process.env.DB_URL
   }
-};
+];
+
+
+const currentEnv = environnements.find(el => el.name === env.toLocaleLowerCase());
+export default { currentEnv, env };
