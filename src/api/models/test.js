215dd8d1f404e316a04b81f23883c9799daf2f60
alain burindi <alainburindi62@gmail.com>
2019-06-10 16:02:22 +0200

    [Ft #166240818] send a valid token to the user

--- src/api/models/test.js
+++
@@ -1,9 +0,0 @@
-const test = (sequelize, DataTypes) => {
-  const Test = sequelize.define('test', {
-    message: DataTypes.STRING,
-  });
-
-  return Test;
-};
-
-export default test;
---
+++ src/helpers/tokens/generate.token.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import models from '../../api/models';
+
+const { Token } = models;
+
+const createToken = (user, secret) => {
+  try {
+    const token = jwt.sign(
+      { user },
+      secret,
+      {
+        expiresIn: '24h'
+      }
+    );
+
+    Token.create({
+      data: token,
+    });
+
+    return token;
+  } catch (error) {
+    return 'error while processing a token';
+  }
+};
+
+export default createToken;
