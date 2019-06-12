a439d37819223917592da083d6199746298a33d1
nkpremices <premices.tuvere@gmail.com>
2019-06-12 18:34:28 +0200

    [Feature #166240823] Resolve copnflicts with develop

--- src/configs/environments.js
+++ src/configs/environments.js
@@ -5,12 +5,9 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV;
 const jwtSecret = process.env.TOKEN_KEY;
-<<<<<<< HEAD:src/configs/environnements.js
 const mailerEmail = process.env.MAILER_EMAIL;
 const mailerToken = process.env.MAILER_API_KEY;
 const baseUrl = process.env.API_BASE_URL;
-=======
->>>>>>> [Ft #166240818] send a valid token to the user:src/configs/environments.js
 
 const environnements = [
   {
@@ -18,47 +15,35 @@ const environnements = [
     port,
     dbUrl: process.env.TEST_DB_URL,
     secret: jwtSecret,
-<<<<<<< HEAD:src/configs/environnements.js
     mailerEmail,
     baseUrl,
-=======
->>>>>>> [Ft #166240818] send a valid token to the user:src/configs/environments.js
   },
   {
     name: 'development',
     port,
     dbUrl: process.env.DEV_DB_URL,
     secret: jwtSecret,
-<<<<<<< HEAD:src/configs/environnements.js
     mailerEmail,
     mailerToken,
     baseUrl,
-=======
->>>>>>> [Ft #166240818] send a valid token to the user:src/configs/environments.js
   },
   {
     name: 'production',
     port,
     dbUrl: process.env.DB_URL,
     secret: jwtSecret,
-<<<<<<< HEAD:src/configs/environnements.js
     mailerEmail,
     mailerToken,
     baseUrl,
-=======
->>>>>>> [Ft #166240818] send a valid token to the user:src/configs/environments.js
   },
   {
     name: 'stagging',
     port,
     dbUrl: process.env.DB_URL,
     secret: jwtSecret,
-<<<<<<< HEAD:src/configs/environnements.js
     mailerEmail,
     mailerToken,
     baseUrl,
-=======
->>>>>>> [Ft #166240818] send a valid token to the user:src/configs/environments.js
   }
 ];
 
