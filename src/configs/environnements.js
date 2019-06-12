99bebc9acdf5b16ee966ffddba72bffc563cecb4
nkpremices <premices.tuvere@gmail.com>
2019-06-12 14:33:46 +0200

    [Feature #166240823] Update configuration files for the password reset endpoint

--- src/configs/environnements.js
+++ src/configs/environnements.js
@@ -4,27 +4,46 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV;
+const jwtSecret = process.env.TOKEN_KEY;
+const mailerEmail = process.env.MAILER_EMAIL;
+const mailerToken = process.env.MAILER_API_KEY;
+const baseUrl = process.env.API_BASE_URL;
 
 const environnements = [
   {
     name: 'test',
     port,
-    dbUrl: process.env.TEST_DB_URL
+    dbUrl: process.env.TEST_DB_URL,
+    secret: jwtSecret,
+    mailerEmail,
+    baseUrl,
   },
   {
     name: 'development',
     port,
-    dbUrl: process.env.DEV_DB_URL
+    dbUrl: process.env.DEV_DB_URL,
+    secret: jwtSecret,
+    mailerEmail,
+    mailerToken,
+    baseUrl,
   },
   {
     name: 'production',
     port,
-    dbUrl: process.env.DB_URL
+    dbUrl: process.env.DB_URL,
+    secret: jwtSecret,
+    mailerEmail,
+    mailerToken,
+    baseUrl,
   },
   {
     name: 'stagging',
     port,
-    dbUrl: process.env.DB_URL
+    dbUrl: process.env.DB_URL,
+    secret: jwtSecret,
+    mailerEmail,
+    mailerToken,
+    baseUrl,
   }
 ];
 
