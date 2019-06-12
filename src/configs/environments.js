a65f46242f0288951f7e0ee5fde4d0c0ece8958d
nkpremices <premices.tuvere@gmail.com>
2019-06-12 14:30:30 +0200

    [Feature #166240823] Add password reset routes, controllers, helpers and middlewares

--- src/configs/environments.js
+++ src/configs/environments.js
@@ -5,33 +5,49 @@ dotenv.config();
 const port = process.env.PORT;
 const env = process.env.NODE_ENV;
 const jwtSecret = process.env.TOKEN_KEY;
+const mailerEmail = process.env.MAILER_EMAIL;
+const mailerToken = process.env.MAILER_API_KEY;
+const baseUrl = process.env.API_BASE_URL;
 
 const environnements = [
   {
     name: 'test',
     port,
     dbUrl: process.env.TEST_DB_URL,
-    secret: jwtSecret
+    secret: jwtSecret,
+    mailerEmail,
+    mailerToken,
+    baseUrl,
   },
   {
     name: 'development',
     port,
     dbUrl: process.env.DEV_DB_URL,
-    secret: jwtSecret
+    secret: jwtSecret,
+    mailerEmail,
+    mailerToken,
+    baseUrl,
   },
   {
     name: 'production',
     port,
     dbUrl: process.env.DB_URL,
-    secret: jwtSecret
+    secret: jwtSecret,
+    mailerEmail,
+    mailerToken,
+    baseUrl,
   },
   {
     name: 'stagging',
     port,
     dbUrl: process.env.DB_URL,
-    secret: jwtSecret
+    secret: jwtSecret,
+    mailerEmail,
+    mailerToken,
+    baseUrl,
   }
 ];
+
 
 const currentEnv = environnements.find(el => el.name === env.toLocaleLowerCase());
 export default { currentEnv, env };
