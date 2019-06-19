3bf0930c2fc665e8151a056a62cf160e13cf9b84
alain burindi <alainburindi62@gmail.com>
2019-06-19 17:02:42 +0200

    allow to raise PR

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import authRouter from './authRouter';
+import articleRouter from './articles';
 
 const router = express();
 
 router.use('/auth', authRouter);
+router.use('/artcle', articleRouter);
 
 export default router;
